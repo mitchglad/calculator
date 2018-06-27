0927d70aceadf39fba33c8f309e20bf213b2bddf
Mitchell Gladstone <mitchell.gladstone@asos.com>
2018-06-27 14:35:36 +0100

    Revert "new feature added"
    This reverts commit a102c60a7529655f175088b4d9f7ca652577610f.

--- Program.cs
+++ Program.cs
@@ -7,9 +7,6 @@ namespace calc
         static void Main(string[] args)
         {
             Console.WriteLine("Calc V1");
-            while(true){
-                Console.WriteLine("LOOOPPPP");
-            }
         }
     }
 }
