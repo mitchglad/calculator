a515ad5bcc67ca0a49a81be84cd5cd6eea84a0c9
Mitchell Gladstone <mitchell.gladstone@asos.com>
2018-06-27 15:25:18 +0100

    bug fix

--- Program.cs
+++ Program.cs
@@ -4,10 +4,15 @@ namespace CoolCalc
 {
     class Program
     {
+
+        public static void SomeImptBugFix(){
+            //all bugs fixed
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Calc V1");
             Console.WriteLine("HELP");
+            SomeImptBugFix();
         }
     }
 }
